f94e4613d2267e93dfb3ffed81c79a42180a85d8
eux <euxuuu@gmail.com>
2019-05-11 23:44:21 +0800

    Add Font Awesome

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def icon(style, name, text = nil, html_options = {})
+    text, html_options = nil, text if text.is_a?(Hash)
+
+    content_class = "#{style} fa-#{name}"
+    content_class << " #{html_options[:class]}" if html_options.key?(:class)
+    html_options[:class] = content_class
+
+    html = content_tag(:i, nil, html_options)
+    text.blank? ? html : html << ' ' << text.to_s
+  end
 end
