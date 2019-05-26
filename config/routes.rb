be5cc8e73ed7b57b0e71e4f8366065280bdfa8a1
eux <euxuuu@gmail.com>
2019-05-26 22:30:13 +0800

    Add home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resource :page do
+    collection do
+      get :home
+    end
+  end
+
+  root 'pages#home'
 end
