f1cd7f7075dcb95b3bcc74814b6fd91ad50234ff
eux <euxuuu@gmail.com>
2019-07-02 20:34:21 +0800

    Improve setup app name

--- bin/setup_app_name.rb
+++ bin/setup_app_name.rb
@@ -1,3 +1,4 @@
+require 'active_support/core_ext/string/inflections'
 require 'tempfile'
 
 def setup_app_name
@@ -11,26 +12,27 @@ def setup_app_name
   current_name = APP_NAME
   new_name = ARGV.first.sub(param, '')
 
-  Tempfile.open(".#{File.basename(file_path)}", File.dirname(file_path)) do |tempfile|
+  edit(file_path) do |tempfile|
     tempfile << "APP_NAME = '#{new_name}'"
-    FileUtils.mv tempfile.path, file_path
   end
 
-  replace('config/application.rb', camelize(current_name), camelize(new_name))
+  replace('config/application.rb', current_name.camelize, new_name.camelize)
   replace('package.json', current_name, new_name)
 
   puts "\nThe new app name is #{new_name}"
 end
 
 def replace(file, string, new_string)
-  Tempfile.open(".#{File.basename(file)}", File.dirname(file)) do |tempfile|
+  edit(file) do |tempfile|
     File.open(file).each do |line|
-      tempfile.puts line.gsub(string, new_string)
+      tempfile << line.gsub(string, new_string)
     end
-    FileUtils.mv tempfile.path, file
   end
 end
 
-def camelize(string)
-  string.split('_').map(&:capitalize).join
+def edit(file)
+  Tempfile.open(".#{File.basename(file)}", File.dirname(file)) do |tempfile|
+    yield tempfile
+    FileUtils.mv tempfile.path, file
+  end
 end
