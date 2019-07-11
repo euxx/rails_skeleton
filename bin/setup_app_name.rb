3d884b424e10ef33ac68ab865f0c3973ba944f8b
eux <euxuuu@gmail.com>
2019-07-11 22:08:15 +0800

    Fix setup app name

--- bin/setup_app_name.rb
+++ bin/setup_app_name.rb
@@ -5,7 +5,7 @@ def setup_app_name
   param = 'app_name='
   return unless ARGV.first&.start_with?(param)
 
-  puts "\n== Set app name =="
+  puts "== Set app name =="
 
   file_path = 'config/initializers/_app_name.rb'
   require_relative "../#{file_path}"
@@ -17,9 +17,15 @@ def setup_app_name
   end
 
   replace('config/application.rb', current_name.camelize, new_name.camelize)
+  replace('config/database.yml', current_name.upcase, new_name.upcase)
+  replace('config/environments/production.rb', current_name, new_name)
+  replace('config/database.yml', current_name, new_name)
+  replace('config/cable.yml', current_name, new_name)
   replace('package.json', current_name, new_name)
 
-  puts "\nThe new app name is #{new_name}"
+  system('git add .')
+  system("git commit -m 'Set app name to #{new_name}'")
+  puts
 end
 
 def replace(file, string, new_string)
