c60cfcfa15af2e44a5f53873d627baf34bf7e977
eux <euxuuu@gmail.com>
2019-07-22 22:51:42 +0800

    Implement sign in/out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :current_user_session
+
+  private
+
+  def current_user
+    @current_user ||= current_user_session&.user
+  end
+
+  def current_user_session
+    @current_user_session ||= UserSession.find
+  end
+
+  def require_blank_user_session
+    redirect_to root_path if current_user
+  end
 end
---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,27 @@
+class UserSessionsController < ApplicationController
+  before_action :require_blank_user_session, only: [:new, :create]
+
+  def new
+    @user_session = UserSession.new
+  end
+
+  def create
+    @user_session = UserSession.new(user_session_params.to_h)
+    if @user_session.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    current_user_session&.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def user_session_params
+    params.require(:user_session).permit(:email, :password, :remember_me)
+  end
+end
