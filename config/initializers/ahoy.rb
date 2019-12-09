7c120bae71611d7e45ea86a2367056e30d1b9f07
eux <euxuuu@gmail.com>
2019-12-09 23:56:25 +0800

    Config ahoy

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -3,3 +3,12 @@ end
 
 # set to true for JavaScript tracking
 Ahoy.api = false
+
+Ahoy.track_bots = true
+Ahoy.cookie_domain = :all
+Ahoy.visit_duration = 6.hours
+
+IPS = %w[192.168.1.1]
+Ahoy.exclude_method = lambda do |controller, request|
+  request.ip.in?(IPS)
+end
