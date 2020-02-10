aa3cc956ff2fa92e3be3ac32a55b585bce5d618c
eux <euxuuu@gmail.com>
2020-02-10 23:13:58 +0800

    Add a filter method: require_sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,12 @@ class ApplicationController < ActionController::Base
     @current_user_session ||= UserSession.find
   end
 
+  def require_sign_in
+    redirect_to root_path, notice: 'Require sign in' unless current_user
+  end
+
   def require_blank_user_session
-    redirect_to root_path if current_user
+    redirect_to root_path, notice: 'Already signed in' if current_user
   end
 
   def set_raven_context
