77c8e74b6f3e7f155e6e511ea5dcef1aea34fdfc
eux <euxuuu@gmail.com>
2020-08-30 00:53:14 +0800

    Disable the updating of User#last_request_at

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,10 @@ class ApplicationController < ActionController::Base
     @current_user_session ||= UserSession.find
   end
 
+  def last_request_update_allowed?
+    false
+  end
+
   def require_sign_in
     redirect_to root_path, notice: 'Require sign in' unless current_user
   end
