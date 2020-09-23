3ca10905fb0242fd87f89b25222c74e96bc887bd
eux <euxuuu@gmail.com>
2020-09-23 23:10:46 +0800

    Remove unnecessary page route

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,6 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   root 'pages#home'
 
-  resource :page, only: [] do
-    collection do
-      get :home
-    end
-  end
-
   get    'sign_in'  => 'user_sessions#new'
   delete 'sign_out' => 'user_sessions#destroy'
 
--- test/system/pages_test.rb
+++ test/system/pages_test.rb
@@ -2,7 +2,7 @@ require "application_system_test_case"
 
 class PagesTest < ApplicationSystemTestCase
   test "visiting the home page" do
-    visit home_page_path
+    visit root_path
     assert_text 'Hello, Stimulus!'
 
     find('span', text: 'Popover').click
