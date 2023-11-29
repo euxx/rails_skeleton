bd7a8fa51175e25ac5fdd59819312270f73eb39b
eux <euxuuu@gmail.com>
2023-11-29 17:52:27 +0800

    Fix ujs start

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -7,7 +7,9 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-require("@rails/ujs").start()
+import Rails from '@rails/ujs'
+Rails.start()
+
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
