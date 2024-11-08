ebe9bf3e5658ae708c2a67efced8d3a91fb9cfb0
eux <euxuuu@gmail.com>
2024-11-08 16:36:44 +0800

    Fix Font Awesome icons display

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,5 @@
 // Entry point for the build script in your package.json
+import '@fortawesome/fontawesome-free/js/all.min'
 import '@hotwired/turbo-rails'
 import './controllers'
 import * as bootstrap from 'bootstrap/dist/js/bootstrap.min.js'
