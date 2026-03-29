68afd8f6321a1e38defc514cd7a09ea6e5523e06
eux <euxx@users.noreply.github.com>
2026-03-29 15:24:56 +0800

    test: increase Capybara wait time to 10s in CI for slow Chrome headless

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   if ENV['CI'] == 'true'
+    Capybara.default_max_wait_time = 10
     driven_by :selenium_chrome_headless, using: :chrome, screen_size: [1400, 1400]
   else
     driven_by :selenium, using: :firefox, screen_size: [1400, 1400]
